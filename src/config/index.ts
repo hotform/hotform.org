cb59541ad94644e5d7c3e7f9f80729af7c9ecc6f
Derry Sucari <derrysucari01@gmail.com>
2023-03-12 16:34:59 -0500

    add configuration

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  classNamePrefix: process.env.GATSBY_APP_CLASS_NAME_PREFIX ?? 'prefix',
+  language: process.env.GATSBY_LANGUAGE ?? 'en',
+  name: process.env.GATSBY_APP_NAME ?? '...'
+};
