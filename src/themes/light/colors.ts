f0e87abb37a68bdead61884fdd41f4bbd1b4ab6b
Derry Sucari <derrysucari01@gmail.com>
2023-03-13 01:20:36 -0500

    add light theme colors

---
+++ src/themes/light/colors.ts
@@ -0,0 +1,22 @@
+import { ThemeColors } from '@themes/shared';
+
+const value: ThemeColors = {
+  background: '#FEFEFE',
+  divider: '#D8DEE4',
+  primary: {
+    contrastText: '#F5F5F5',
+    main: '#040D21'
+  },
+  secondary: {
+    contrastText: '#F3F3F3',
+    main: '#1C2333'
+  },
+  text: {
+    primary: '#040D21DE',
+    secondary: '#040D2199',
+    link: '#0969DA',
+    disabled: '#040D2161'
+  }
+};
+
+export default value;
