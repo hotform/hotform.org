f6faaccf989be3fe84fc99bebe1d3d5b7414c819
Derry Sucari <derrysucari01@gmail.com>
2023-03-17 04:22:00 -0500

    update configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,12 @@
 export default {
   classNamePrefix: process.env.GATSBY_APP_CLASS_NAME_PREFIX ?? 'prefix',
   language: process.env.GATSBY_LANGUAGE ?? 'en',
-  name: process.env.GATSBY_APP_NAME ?? '...'
+  name: process.env.GATSBY_APP_NAME ?? '...',
+  socialMedia: [
+    {
+      label: 'GitHub',
+      to: 'https://github.com/hotform/hotform'
+    }
+  ],
+  repositoryURL: 'https://github.com/hotform/hotform.org'
 };
