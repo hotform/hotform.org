755c480c4c1395a85eaa71db42d002529baafc3c
Derry Sucari <derrysucari01@gmail.com>
2023-04-11 16:03:54 -0500

    update `useMediaQuery hook`

--- src/themes/shared/hooks/useMediaQuery.ts
+++ src/themes/shared/hooks/useMediaQuery.ts
@@ -1,24 +1,59 @@
 import React from 'react';
 
-const useMediaQuery = (query: string): boolean => {
-  const [ match, setMatch ] = React.useState(false);
+export interface UseMediaQueryConfig{
+  defaultMatches?: boolean;
+  matchMedia?: typeof window.matchMedia;
+  noSsr?: boolean;
+  query: string;
+}
+
+const matcher: RegExp = /^@media  ?/m;
+
+const useMediaQuery = ({
+  query: queryInput,
+  defaultMatches = false,
+  matchMedia = typeof window === 'undefined' ? undefined : window.matchMedia,
+  noSsr = false
+}: UseMediaQueryConfig): boolean => {
+  const query = queryInput.replace(matcher, '');
   
-  if(typeof window === 'undefined') return false;
+  const getDefaultSnapshot = React.useCallback(() => defaultMatches, [defaultMatches]);
   
-  const handleChange = React.useCallback((e: MediaQueryListEvent) => setMatch(e.matches), []);
+  const getServerSnapshot = React.useMemo(() => (
+    noSsr && matchMedia
+      ? () => matchMedia(query).matches
+      : getDefaultSnapshot
+  ), [
+    getDefaultSnapshot,
+    matchMedia,
+    noSsr,
+    query
+  ]);
   
-  React.useEffect(() => {
-    const mediaQueryList = matchMedia(query.replace(/^@media  ?/m, ''));
-    
-    setMatch(mediaQueryList.matches);
-    
-    mediaQueryList.addEventListener('change', handleChange);
-    return () => {
-      mediaQueryList.removeEventListener('change', handleChange);
+  const [ getSnapshot, subscribe ] = React.useMemo(() => {
+    if(matchMedia){
+      const mediaQueryList = matchMedia(query);
+      return [
+        () => mediaQueryList.matches,
+        (listener: (e: MediaQueryListEvent) => void) => {
+          mediaQueryList.addEventListener('change', listener);
+          return () => {
+            mediaQueryList.removeEventListener('change', listener);
+          };
+        },
+      ];
     }
-  }, [ query, handleChange ]);
+    return [
+      getDefaultSnapshot,
+      () => () => {}
+    ];
+  }, [
+    getDefaultSnapshot,
+    matchMedia,
+    query
+  ]);
   
-  return match;
+  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 }
 
 export default useMediaQuery;
